d6fa11ea95fee646c4c738d340db161eff5dd745
miki-matsumoto <mikimatsumoto829@gmail.com>
2022-10-20 20:36:42 +0900

    WIP

--- src/lib/trpc/server/createContext.ts
+++ src/lib/trpc/server/createContext.ts
@@ -10,14 +10,11 @@ export const createContext = async (
 
 export type Context = trpc.inferAsyncReturnType<typeof createContext>;
 
+// For test
 interface CreateContextOptions {
   // session: Session | null
 }
 
-/**
- * Inner function for `createContext` where we create the context.
- * This is useful for testing when we don't want to mock Next.js' request/response
- */
 export async function createContextInner(_opts: CreateContextOptions) {
   return {};
 }
